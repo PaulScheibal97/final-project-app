c3c65b577841fcefb79488c73b55d29b38e19c6a
PaulScheibal97 <143520889+PaulScheibal97@users.noreply.github.com>
2023-12-20 11:22:01 -0600

    re formatted page layout

--- src/App.jsx
+++ src/App.jsx
@@ -219,18 +219,17 @@ function App() {
              <Card card={card} removeCard={removeCard} updateCard={updateCard} />
            </div>
          ))}
-         <div className="row d-flex justify-content-center">
+        <div className="row d-flex justify-content-center">
+        <div className="d-flex justify-content-around" id="iconDescription">
+              <div id="gem" className="d-flex justify-content-center"><div id="test"><FontAwesomeIcon icon={faGem} id="spinGem" flip /></div><p id="spinGemText">Rarity</p></div>
+              <div id="fist" className="d-flex justify-content-center"><div><FontAwesomeIcon icon={faHandFist} id="spinFist" flip /></div><p id="spinFistText">Power Level</p></div>
+              <div id="shield" className="d-flex justify-content-center"><div><FontAwesomeIcon icon={faShieldCat} id="spinShield" flip /></div><p id="spinShieldText">Silly Level</p></div>
+           </div>
           <div className="col-sm-12 d-flex justify-content-center">
             <AddCard addCard={addCard} />
-          </div>
-          <div className="d-flex flex-column" id="iconDescription">
-          <div id="gem" className="d-flex justify-content-center"><div id="test"><FontAwesomeIcon icon={faGem} id="spinGem" flip /></div><p id="spinGemText">Rarity</p></div>
-          <div id="fist" className="d-flex justify-content-center"><div><FontAwesomeIcon icon={faHandFist} id="spinFist" flip /></div><p id="spinFistText">Power Level</p></div>
-          <div id="shield" className="d-flex justify-content-center"><div><FontAwesomeIcon icon={faShieldCat} id="spinShield" flip /></div><p id="spinShieldText">Silly Level</p></div>
-        </div>
          </div>
+        </div>
       </div>
-      
    </div>
   </div>
  );
